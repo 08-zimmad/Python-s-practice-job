89166ca50be362d68b9368515b7422faf400ea80
08-zimmad <zimmad.w@gmail.com>
2024-04-02 18:20:07 +0500

    added comments

--- palindrome_int.py
+++ palindrome_int.py
@@ -1,8 +1,8 @@
 
 # using strings
-def isPalindrome(x: int) -> bool:
-    str_num=str(x)
-    str_num=str_num[::-1]
+def isPalindrome(x: int) -> bool: # returns Bool value
+    str_num=str(x) 
+    str_num=str_num[::-1] # this slicing is used for reversing the list/string
     return str(x)==str_num
 
 
@@ -12,15 +12,13 @@ def isPalindrome(x: int) -> bool:
 def isPalindrome2(x: int) -> bool:
     if x<0:
         return False
-    num=x
-    reverse=0
+    num=x   
+    reverse=0   # for storing the reversed integer
     while num!=0:
-        digit=num%10
-        reverse=reverse*10+digit
-        num=num//10
-    if x==reverse:
-        return True
-    return False
+        digit=num%10  # eg 121 ,, the % would give the reminder which is 1
+        reverse=reverse*10+digit # initialy: 0=0*10+1 -> 1
+        num=num//10 # 121//10=12
+    return reverse==x
 
 
 print(isPalindrome2(-121))
