fb83aca33cbf9b0909c07d3b1c22d2115ac02801
08-zimmad <zimmad.w@gmail.com>
2024-04-23 10:54:51 +0500

    update decorator

--- Arrays and lists/decorater.py
+++ Arrays and lists/decorater.py
@@ -1,4 +1,4 @@
-def my_decorator(func):
+def check(func):
     def wrapper():
         print("Something is happening before the function is called.")
         func()
@@ -10,11 +10,25 @@ def say_hello():
     print("Hello!")
 
 
-say_hello = my_decorator(say_hello)
+say_hello1 = check(say_hello)
 
-say_hello()
+say_hello1()
+
+
+
+# following is the code for the using @decorator
+
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
 
 
 @my_decorator
 def say_hello():
     print("Hello!")
+
+
+say_hello()
