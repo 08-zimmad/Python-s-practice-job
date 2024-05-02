703fd58c9161f7fd33e6a74dad135fbc1f159e5b
08-zimmad <zimmad.w@gmail.com>
2024-05-02 16:24:10 +0500

    Create perfect_no.py

---
+++ Arrays and lists/perfect_no.py
@@ -0,0 +1,14 @@
+# sum of divisors if equal to the given number then its perfect number
+
+def Chk_Perfect(n):
+    sum=0
+    for i in range (1,((n//2)+1)):
+        rem=n%i
+        if rem==0:
+            sum=sum+i
+    if sum==n:
+        print("it is a perfect square")
+    else:
+        print("It is not a perfect square")
+ 
+Chk_Perfect(8128)
