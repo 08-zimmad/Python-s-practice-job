105eca20a12a42656fce6f8a0f5a0b9d68e2ab6a
zimmadw <zimmad.w@gmail.com>
2026-02-02 18:09:39 +0500

    Reversal of string with space preserver

---
+++ two_pointer/reversal_with_space_presevre.py
@@ -0,0 +1,24 @@
+def preserveSpace(Str):
+    n = len(Str)
+    Str = list(Str)
+
+    start = 0
+    end = n - 1
+
+    while start < end:
+        if Str[start] == " ":
+            start += 1
+            continue
+        elif Str[end] == " ":
+            end -= 1
+            continue
+
+        else:
+            Str[start], Str[end] = (Str[end], Str[start])
+            start += 1
+            end -= 1
+    return "".join(Str)
+
+
+s = "internship at geeks for geeks"
+print(preserveSpace(s))
